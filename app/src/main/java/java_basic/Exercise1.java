4d208cde1ca86f7945560c52331d7b4143736c30
Tung <mieo.mieo.1102@gmail.com>
2024-03-08 14:55:35 +0700

    ex4 update

--- app/src/main/java/java_basic/Exercise1.java
+++ app/src/main/java/java_basic/Exercise1.java
@@ -6,8 +6,10 @@ public class Exercise1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double score = sc.nextDouble();
-        if(score >= 5){
-
+        if(score>10 || score <0){
+            System.out.println("Wrong number");
+        }
+        else if(score >= 5){
             System.out.print("Pass");
         }
         else{
@@ -15,5 +17,4 @@ public class Exercise1 {
         }
         sc.close();
     }
-
 }
