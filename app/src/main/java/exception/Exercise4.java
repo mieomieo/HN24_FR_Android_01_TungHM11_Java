b97bcb9a888b37557d7a06df6913b8a6830e4713
Tung <mieo.mieo.1102@gmail.com>
2024-03-08 09:37:27 +0700

    ex4 update

--- app/src/main/java/exception/Exercise4.java
+++ app/src/main/java/exception/Exercise4.java
@@ -4,10 +4,23 @@ public class Exercise4 {
     public static void main(String[] args) {
         String str = "abc";
         try {
-            double num = Double.parseDouble(str);
-            System.out.println("Số: " + num);
+            int intValue = Integer.parseInt(str);
+            System.out.println("Số nguyên: " + intValue);
         } catch (NumberFormatException e) {
-            System.out.println("Lỗi: Không thể ép kiểu chuỗi sang số.");
+            System.out.println("Lỗi: Không thể ép kiểu chuỗi sang số nguyên.");
         }
+        try {
+            double doubleValue = Double.parseDouble(str);
+            System.out.println("Số thực: " + doubleValue);
+        } catch (NumberFormatException e) {
+            System.out.println("Lỗi: Không thể ép kiểu chuỗi sang số thực.");
+        }
+        try {
+            long longValue = Long.parseLong(str);
+            System.out.println("Số long: " + longValue);
+        } catch (NumberFormatException e) {
+            System.out.println("Lỗi: Không thể ép kiểu chuỗi sang số long.");
+        }
+
     }
 }
