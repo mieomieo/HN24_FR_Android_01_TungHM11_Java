b91a11e657159be7da94ed5f2fdd529287b3cc65
Tung <mieo.mieo.1102@gmail.com>
2024-03-08 09:23:39 +0700

    ex3 update

--- app/src/main/java/exception/Exercise3.java
+++ app/src/main/java/exception/Exercise3.java
@@ -4,7 +4,7 @@ public class Exercise3 {
     public static void main(String[] args) {
         int arr[]= new int[5];
         try {
-            System.out.println(arr[7]);
+            System.out.println(arr[6]);
         }catch (ArrayIndexOutOfBoundsException  e){
             System.out.println("Lỗi: Phần tử không tồn tại trong mảng.");
         }
